481de38df65e320ed6d0d959f4f153c9b3134d86
Phillip James <phillipjam@gmail.com>
2017-08-13 14:19:07 -0500

    open user registrtion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < Admin::ApplicationController
-  before_action :verify_logged_in
+  # before_action :verify_logged_in
     def new
     @page_title = 'Add New User'
     @user = User.new
